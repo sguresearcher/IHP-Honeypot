df714bbfa37f0b65bf52216d4f08c2dc5bee9e0e
sguresearcher <sguresearcher@gmail.com>
2024-08-09 08:28:39 +0000

    DLP Script v2.2

---
+++ hpot_v2.2/installer-script_v2.2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERNAME="hpot"
+
+while true; do
+    read -sp "Enter password for user $USERNAME: " PASSWORD
+    echo
+    read -sp "Confirm password for user $USERNAME: " PASSWORD_CONFIRM
+    echo
+
+    if [ "$PASSWORD" == "$PASSWORD_CONFIRM" ]; then
+        break
+    else
+        echo "Passwords do not match, please try again."
+    fi
+done
+
+if id "$USERNAME" &>/dev/null; then
+    echo "User $USERNAME already exists."
+else
+    echo "Creating user $USERNAME..."
+    sudo useradd -m -s /bin/bash $USERNAME
+    echo "$USERNAME:$PASSWORD" | sudo chpasswd
+    sudo usermod -aG sudo $USERNAME
+fi
+
+sudo su - $USERNAME -c '
+cd ~
+echo "Running as $(whoami)..."
+wget https://raw.githubusercontent.com/sguresearcher/IHP-Honeypot/main/script_v2.1.sh
+chmod +x ~/script_v2.1.sh
+'
